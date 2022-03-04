5928f063236846d326535189d7ebd09426601b4f
Eddiemn <eddiemn@hotmail.com>
2022-03-04 14:52:28 -0500

    add exe2vendredi

---
+++ exe2vendredi.py
@@ -0,0 +1,23 @@
+def addition(fraction1, fraction2):
+    return fraction1 + fraction2
+
+def soustraction(fraction1, fraction2):
+    return fraction1 - fraction2
+
+def multiplication(multi1, multi2):
+    return multi1, multi2
+
+def division(fraction1, fraction2):
+    return fraction1 / fraction2
+
+
+numerateur1 = 1
+denominateur1 = 6
+numerateur2 = 3
+denominateur2 = 6
+
+multi1 = numerateur1 * numerateur2
+multi2 = denominateur1 * denominateur2
+
+print(multi1)
+print(multi2)
